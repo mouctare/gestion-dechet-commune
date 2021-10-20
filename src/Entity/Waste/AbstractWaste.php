daf8e39a2e46b5bb246ed5869b863d4dd2176b23
mouctare <mouctard78@gmail.com>
2021-10-20 11:21:01 +0200

    addition of the methodological file

--- src/Entity/Waste/AbstractWaste.php
+++ src/Entity/Waste/AbstractWaste.php
@@ -5,14 +5,34 @@ namespace App\Entity\Waste;
 abstract class AbstractWaste
 {
     
-   protected integer $quantity;
-
-    public function __construct($quantity)
-    {
-        
-        $this->$quatity = $quantity;
-        
-    }
-
+    protected $co2Emission = [];
+    protected $quantity = [];
     
+
+  public function getQuantity(): array
+  {
+    return $this->quantity;
+  }
+
+  public function addQuantity(array $quantities)
+  {
+   
+  }
+
+  public function removeQuantity(array $quantities)
+  {
+  
+  }
+
+  public function getCo2Emission(): array
+  {
+    return $this->co2Emission;
+  }
+
+  public function setCo2Emission(array $co2Emissions)
+  {
+   
+  }
+
+  
 }
